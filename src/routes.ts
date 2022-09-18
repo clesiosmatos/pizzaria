5112d6eea1ef9a262444be11cbc5ddf5d7e4ea57
Clesio Santana <clesiosmatos@gmail.com>
2022-09-18 20:53:36 -0300

    Criando lista de produtos por categoria

--- src/routes.ts
+++ src/routes.ts
@@ -8,7 +8,9 @@ import { DetailUserController } from './controllers/user/DetailUserController';
 
 import { CreateCategoryController } from './controllers/category/CreateCategoryController';
 import { ListCategoryController } from './controllers/category/ListCategoryController';
+
 import { CreateProductController } from './controllers/product/CreateProductController';
+import { ListByCategoryController } from './controllers/product/ListByCategoryController';
 
 import uploadConfig from './config/multer';
 
@@ -30,5 +32,6 @@ router.get('/category', isAuthenticated, new ListCategoryController().handle);
 
 //--ROTAS PRODUCT
 router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle);
+router.get('/category/product', isAuthenticated, new ListByCategoryController().handle);
 
 export { router };
